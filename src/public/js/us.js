eacd62fecfaa7d7680665fd7cb44aa5c0f1a38f8
PanchoPC2 <58098908+Panchober27@users.noreply.github.com>
2021-07-08 16:29:05 -0400

    Añadi Jquery de manera local y peleo con jquery en us.js jajja

--- src/public/js/us.js
+++ src/public/js/us.js
@@ -1,4 +1,23 @@
 // Archivo de Jquery para la pagina de presentacion.
-$(document).ready(() => {
-    // $('#contenedor').addClass('animate__animated animate__bounceOutLeft')
+$(() => {
+  $(".card").addClass("animate__animated animate__flipInY")
+  const cards = $(".card")
+
+  const card1 = $("#c1")
+  const card2 = $("#c2")
+
+  const c1txt = $("#ctxt1")
+  const c2txt = $("#ctxt2")
+
+  const msg = "Pancho es dios"
+
+
+
+  
+  cards.on("animationend", () => {
+      // card2.innerHTML = "Pancho no es dios"
+      // alert("end of animation")
+      c1txt.text = msg
+  })
+
 })
