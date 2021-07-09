d5a9fe6598c4a4fe842d33954f1b689e6ecf4540
PanchoPC2 <58098908+Panchober27@users.noreply.github.com>
2021-07-08 23:27:09 -0400

    Casi Casi

---
+++ src/public/js/services.js
@@ -0,0 +1,9 @@
+//
+$(() => {
+  const rowCards = $('#head-box')
+  console.log(rowCards)
+  rowCards.css({display: 'flex', justifyContent: 'center'})
+
+  
+
+})
--- src/public/js/us.js
+++ src/public/js/us.js
@@ -3,10 +3,16 @@ $(() => {
   const animatedCard1 = $("#c1")
   const animatedCard2 = $("#c2")
 
+  const noAnimCard = $('#nonanim')
+
+  noAnimCard.css({transition: 'none',hover: false})
+
   animatedCard1.addClass("animate__animated animate__flipInY")
   animatedCard2.addClass("animate__animated animate__flipInY")
 
   $("#draggable").draggable()
+
+
 
 
 })
