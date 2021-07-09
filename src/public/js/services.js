a1726d1519b02cb8b19914bd8b2df8261fca4585
PanchoPC2 <58098908+Panchober27@users.noreply.github.com>
2021-07-08 23:45:05 -0400

    Final commit

--- src/public/js/services.js
+++ src/public/js/services.js
@@ -1,9 +1,18 @@
 //
 $(() => {
-  const rowCards = $('#head-box')
+  const rowCards = $("#head-box")
+  const cards = $(".card")
   console.log(rowCards)
-  rowCards.css({display: 'flex', justifyContent: 'center'})
+  rowCards.css({ display: "flex", justifyContent: "center" })
+  console.log(cards)
 
-  
+  cards1.on("mouseenter", () => {
+    cards.css({
+      background: 'rgba(0, 0, 0, 0.2)',
+      transition: "1s",
+      color: "white",
+      boxShadow: '4px 4px 4px 5px black'
+    })
+  })
 
 })
